321cf541f318c30e1cc9a5a4c1999b7edfa80085
akash <akash.ju.msc@gmail.com>
2026-07-02 12:55:11 +0600

    week-05: modified user resource

--- capstone-project/routes/api.php
+++ capstone-project/routes/api.php
@@ -5,3 +5,7 @@ use App\Http\Controllers\Api\AuthController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function() {
+    Route::put('/profile', [AuthController::class, 'update']);
+});
