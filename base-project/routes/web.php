3a0777f3ac3c9eab09014cbfc1cba2c7e47d6e62
akash <akash.ju.msc@gmail.com>
2026-07-11 00:28:00 +0600

    feat(sanctum): added sanctum

--- base-project/routes/web.php
+++ base-project/routes/web.php
@@ -1,35 +1,20 @@
 <?php
 
-use App\Services\TestService;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
-//echo __DIR__;
-//echo "<br>";
-//echo dirname(__DIR__);
-//echo "<br>";
-//echo PHP_EOL;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test', function() {
-    $test = new TestService();
-    return $test->hello();
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/hello', function() {
-    return response()->json([
-        'name' => "anaya",
-        'role' => 'student'
-    ]);
-});
-
-// three closure route
-Route::get('/a', fn () => 'A');
-Route::get('/b', fn () => 'B');
-Route::get('/c', fn () => 'C');
-
-Route::get('/home', [\App\Http\Controllers\PageController::class, 'home']);
-Route::get('/about', [\App\Http\Controllers\PageController::class, 'about']);
-Route::get('/contact', [\App\Http\Controllers\PageController::class, 'contact']);
+require __DIR__.'/auth.php';
