b7c5d07db9e50787c0fb7a3f74dee12f6bc4daf1
akash <akash.ju.msc@gmail.com>
2026-07-31 10:16:11 +0600

    refactor(auth): prepare user model

--- ecommerce-api/app/Models/User.php
+++ ecommerce-api/app/Models/User.php
@@ -23,6 +23,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'phone',
+        'avatar',
+        'status',
+        'uuid'
     ];
 
     /**
@@ -45,6 +49,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'status' => 'boolean'
         ];
     }
 }
