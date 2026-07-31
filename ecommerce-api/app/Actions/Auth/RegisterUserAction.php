c6aa9962a72ffdbc613880889effd4414f8057ff
akash <akash.ju.msc@gmail.com>
2026-07-31 15:31:09 +0600

    feat(auth): implement register user action

--- ecommerce-api/app/Actions/Auth/RegisterUserAction.php
+++ ecommerce-api/app/Actions/Auth/RegisterUserAction.php
@@ -2,13 +2,21 @@
 
 namespace App\Actions\Auth;
 
+use App\Models\User;
+use Illuminate\Support\Str;
+
 class RegisterUserAction
 {
-    /**
-     * Create a new class instance.
-     */
-    public function __construct()
+    public function execute(array $data): User
     {
-        //
+        return User::create([
+            'uuid' => Str::uuid(),
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => $data['password'], //automatic cast korbe from cast
+            'phone' => $data['phone'] ?? null,
+            'status' => true,
+            'avatar' => null
+        ]);
     }
 }
